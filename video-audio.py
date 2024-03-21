ca1e04780a87608d07a2113cef612675cc480273
navjyotcodz <95230022+navjyotcodz@users.noreply.github.com>
2024-03-22 00:57:37 +0530

    Update video-audio.py

--- video-audio.py
+++ video-audio.py
@@ -1,7 +1,10 @@
+# Video-To-Audio
+# A simple python tool which converts mp4 files into mp3 files.
+# Author - navjyotcodz
+
 from moviepy.editor import*
 mp4_file="testvideo.mp4"
 mp3_file="navo.mp3"
 videoclip=VideoFileClip(mp4_file)
 audioclip=videoclip.audio
 audioclip.write_audiofile(mp3_file)
-
